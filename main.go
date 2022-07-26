825e831b22c4b04a17f587aa4847e0e1c9211676
devvspaces <netrobepy@gmail.com>
2022-07-27 00:11:13 +0100

    completed part one of project

--- main.go
+++ main.go
@@ -10,9 +10,39 @@ import (
 	"strings"
 )
 
+func get_file_name() string {
+	defer func() {
+		str := recover()
+		if str != nil {
+			fmt.Println("Make sure you passed a file name")
+		}
+	}()
+
+	allowed_flags := []string{
+		"-f",
+	}
+
+	if len(os.Args) > 1 {
+		flag := os.Args[1]
+		found := false
+		for _, value := range allowed_flags {
+			if flag == value {
+				found = true
+				file_name := os.Args[2]
+				return file_name
+			}
+		}
+		if found == false {
+			panic("Invalid Flag used")
+		}
+	}
+
+	return "problems.csv"
+}
+
 func main() {
 	// open file
-	f, err := os.Open("problems.csv")
+	f, err := os.Open(get_file_name())
 	if err != nil {
 		log.Fatal(err)
 	}
